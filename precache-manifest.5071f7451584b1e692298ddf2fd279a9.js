7944a54a5ba7c56941acf893ec3970c1fde11fb3
mohammad jafarkhadem <jkhadem.m@gmail.com>
2020-05-31 18:40:17 +0430

    Updates

---
+++ precache-manifest.5071f7451584b1e692298ddf2fd279a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7509d183ae3a1aecb9a242ec3f6757d3",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "b159ba7c4b2cea3e839f",
+    "url": "/resume/static/css/main.6786f58f.chunk.css"
+  },
+  {
+    "revision": "ff67860475845c7e335a",
+    "url": "/resume/static/js/2.5247780a.chunk.js"
+  },
+  {
+    "revision": "f96c86c6b129b53e00b137c81b7d6545",
+    "url": "/resume/static/js/2.5247780a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b159ba7c4b2cea3e839f",
+    "url": "/resume/static/js/main.2ee7b4c3.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  }
+]);
